df4a3d2e6f2038add40f803c820d1ba6a6c42684
perkinson1251 <tttrashutin@gmail.com>
2025-01-12 16:22:44 +0200

    feat: add csp policies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,23 @@ export default defineNuxtConfig({
       name: 'slide-left',
       mode: 'out-in',
     },
+    head: {
+      meta: [
+        {
+          'http-equiv': 'Content-Security-Policy',
+          content: [
+            "default-src 'self'",
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+            "style-src 'self' 'unsafe-inline'",
+            "img-src 'self' data: https:",
+            "font-src 'self' data:",
+            "connect-src 'self'",
+            "frame-src 'none'",
+            "manifest-src 'self'",
+          ].join('; '),
+        },
+      ],
+    },
   },
   components: [
     {
